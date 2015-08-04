7bdd241630db6ee3dd506eeca2a27150707b0911
zonika <zchodosh@conncoll.edu>
2015-08-04 15:39:35 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word=word
+  end
+  def isanagram(word)
+    w1=@word.split("")
+    w2=word.split("")
+    w1.sort==w2.sort
+  end
+  def match(words)
+    words.select do |word|
+      self.isanagram(word)
+    end
+  end
+end
